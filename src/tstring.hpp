5e20f9da6021344b37bce6e1bb6fd7043998cb66
Stefan Fuhrmann <stefan.fuhrmann@efficientware.net>
2025-04-05 15:15:05 +0200

    Use the same empty string instance for all default-created t_string

--- src/tstring.hpp
+++ src/tstring.hpp
@@ -201,6 +201,8 @@ public:
 	void swap(t_string& other) { val_.swap(other.val_); }
 
 private:
+	static const std::shared_ptr<const t_string_base>& get_empty_string();
+
 	//never null
 	std::shared_ptr<const t_string_base> val_;
 };
